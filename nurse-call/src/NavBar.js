7dca491c65e2fa53704e26a7c63e79ea5a5a4aae
Frederick Ambo <Frederick@tacaly.com>
2021-06-07 21:02:51 +0200

    Adding NavBar
    Added navigation to code

---
+++ nurse-call/src/NavBar.js
@@ -0,0 +1,11 @@
+const NavBar () => {
+    return (
+        <nav className="NavBar">
+            <div className="links">
+                <a href="/">Home</a>
+            </div>
+        </nav>
+    );
+}
+
+export default NavBar;
